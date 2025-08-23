da05b89959a620b7db507b4caf3843148219ac75
Vatsal Trivedi <trivedivatsal005@gmail.com>
2025-08-23 23:14:14 +0530

    Add new line to src/index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const core = require("@actions/core");
 const { Octokit } = require("@octokit/action");
 const { createSystemPrompt, createPromptByDepth } = require("./prompts");
 
+
 async function run() {
   try {
     // Get inputs from action
